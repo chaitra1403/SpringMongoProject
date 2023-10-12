1d89fc9c4b6a51f9dbae4388f26d25b5978c6883
amantripathi01 <amankumartripathi29@gmail.com>
2023-10-12 13:02:06 +0530

    Minor fix

--- src/main/java/com/example/SpringMongoProject/Controller/StudentController.java
+++ src/main/java/com/example/SpringMongoProject/Controller/StudentController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.*;
 public class StudentController {
 
     @Autowired
-    private StudentServices studentServices;
+    public StudentServices studentServices;
 
     @PostMapping(value = "/save")
-    private String saveStudent(@RequestBody Student students) {
+    public String saveStudent(@RequestBody Student students) {
 
         studentServices.saveorUpdate(students);
         return students.get_id();
@@ -26,7 +26,7 @@ public class StudentController {
     }
 
     @PutMapping(value = "/edit/{id}")
-    private Student update(@RequestBody Student student, @PathVariable(name = "id") String _id) {
+    public Student update(@RequestBody Student student, @PathVariable(name = "id") String _id) {
         student.set_id(_id);
         studentServices.saveorUpdate(student);
         return student;
@@ -34,13 +34,13 @@ public class StudentController {
 
 
     @DeleteMapping("/delete/{id}")
-    private void deleteStudent(@PathVariable("id") String _id) {
+    public void deleteStudent(@PathVariable("id") String _id) {
         studentServices.deleteStudent(_id);
     }
 
 
     @RequestMapping("/search/{id}")
-    private Student getStudents(@PathVariable(name = "id") String studentid) {
+    public Student getStudents(@PathVariable(name = "id") String studentid) {
         return studentServices.getStudentByID(studentid);
     }
 
